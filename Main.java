49434a2925e59817ace9386c4a3c28fa399312ea
Tabooretishe <vladimirgreat14@gmail.com>
2024-10-24 09:51:36 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,27 @@
 import java.io.PrintStream;
 import java.util.Scanner;
-
 public class Main {
     public static Scanner in = new Scanner(System.in);
     public static PrintStream out = System.out;
     public static void main(String[] args) {
         int x = in.nextInt();
         boolean Repeat = false;
-        if (x/1000==0 || x/10000>0)
+        if (x < 1000 || x > 9999)
             out.print("NO");
         else {
-            for(int i=3, xi, a1; i>0; i--) {
-                a1=4-i;
-                xi=x;
-                xi = xi / (int)Math.pow(10,i);
-                while(xi>9 && a1>1) {
-                    xi=xi%10;
-                    a1--;
-                }
-                for(int u=0, xu, a2; u<i; u++){
-                    a2=4;
-                    xu=x;
-                    xu = xu / (int)Math.pow(10,u);
-                    while(xu>9 && a2>u) {
-                        xu = xu % 10;
-                        a2--;
-                    }
-                    if (xu == xi)
-                        Repeat = true;
-            }}
-            if (!Repeat)
+            if (x / 1000 == x / 100 % 10)
+                Repeat = true;
+            if (x / 1000 == x / 10 % 10 % 10)
+                Repeat = true;
+            if (x / 1000 == x % 10)
+                Repeat = true;
+            if (x / 100 % 10 == x / 10 % 10 % 10)
+                Repeat = true;
+            if (x / 100 % 10 == x % 10)
+                Repeat = true;
+            if (x % 10 == x / 100 % 10)
+                Repeat = true;
+            if (Repeat == false)
                 out.print("YES");
             else
                 out.print("NO");
